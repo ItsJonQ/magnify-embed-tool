691f8f8c54fae1a49f79284643159ced53791194
ItsJonQ <itsjonq@gmail.com>
2014-02-13 10:54:21 -0500

    Added test.json for development

--- scripts/utils/fetch.js
+++ scripts/utils/fetch.js
@@ -1,14 +1,18 @@
 define(['jquery'], function($) {
     'use strict';
 
-    var apiUrl = 'http://technobuffalo.magnify.net/api/content/find?vq=&per_page=10&key=FN697D1RFGP2VQLR&format=json';
+    // var apiUrl = 'http://technobuffalo.magnify.net/api/content/find?vq=&per_page=10&key=FN697D1RFGP2VQLR&format=json';
+
+    // Test JSON
+    var apiUrl = 'scripts/test/test.json';
 
     var fetch = function(callback) {
         var fetchUrl = apiUrl;
         $.ajax({
             type: "GET",
             url: fetchUrl,
-            dataType: 'jsonp',
+            dataType: 'json',
+            // dataType: 'jsonp',
             success: function(data) {
                 if(callback !== undefined && typeof callback === 'function') {
                     return callback(data);
